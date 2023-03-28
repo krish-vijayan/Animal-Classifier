412781dbbcca7972da5d12a0a95c03f769e39c83
krish-vijayan <krishvijayan55@gmail.com>
2023-03-28 15:17:25 -0400

    Initial Commit

---
+++ Animal Classifier/Animal_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_ClassifierApp.swift
+//  Animal Classifier
+//
+//  Created by Krish Vijayan on 2023-03-28.
+//
+
+import SwiftUI
+
+@main
+struct Animal_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
