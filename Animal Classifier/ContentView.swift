8c8d03abf9117418bdfaa1f9a1afbb4ca5696274
krish-vijayan <krishvijayan55@gmail.com>
2023-03-28 18:15:32 -0400

    Initial Commit

--- Animal Classifier/ContentView.swift
+++ Animal Classifier/ContentView.swift
@@ -6,21 +6,75 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+    @State var imageNum = 0
+    @State var classification = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button(action: nextAnimal){
+                Text("Next Animal")
+            }
+            AsyncImage(url: URL(string: Images.images[imageNum]))
+            { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                Color.gray
+            }
+            .frame(width: 250, height: 250)
+            Text(classification)
         }
         .padding()
+        .onAppear{
+            detectAnimal(image: Images.images[imageNum])
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func nextAnimal() {
+        imageNum += 1
+        if imageNum >= Images.images.count {
+            imageNum = 0
+        }
+        detectAnimal(image: Images.images[imageNum])
+    }
+    func detectAnimal(image: String) {
+        guard let model = try? VNCoreMLModel(for: DogCatRabbitClassifier().model) else {fatalError("Loading CoreML Model Failed")}
+        let request = VNCoreMLRequest(model: model){(request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {return}
+            
+            if let firstResult = results.first {
+                
+                classification = firstResult.identifier
+                print(classification)
+                
+            }
+        }
+        if let url = URL(string: image) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                // Error handling...
+                guard let imageData = data else { return }
+                guard let ciImage = CIImage(data: imageData) else {fatalError("Cannot convert to CIImage")}
+                
+                DispatchQueue.main.async {
+                    
+                    let handler = VNImageRequestHandler(ciImage: ciImage)
+                    
+                    do{
+                        try handler.perform([request])
+                    }catch{
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+        
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ Animal Classifier/Images/Images.swift
@@ -0,0 +1,24 @@
+//
+//  Images.swift
+//  Animal Classifier
+//
+//  Created by Krish Vijayan on 2023-03-28.
+//
+
+import Foundation
+
+struct Images {
+    static let images = [
+        "https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg",
+        "https://img.asmedia.epimg.net/resizer/CInsaWwx8ety0U0kPFnYPuLwIeM=/1952x1098/filters:focal(657x476:667x486)/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/IF2GBNOEH5BXJMOQ2T2ABFEITI.jpg",
+        "https://c.files.bbci.co.uk/295E/production/_100209501_istock-678095904.jpg",
+        "https://cdn.cdnparenting.com/articles/2021/09/22115215/1411747946.jpg",
+        "https://img.etimg.com/thumb/msid-95129274,width-1600,height-1066,imgsize-59990,overlay-economictimes/photo.jpg",
+        "https://alpha.aeon.co/images/acd6897d-9849-4188-92c6-79dabcbcd518/header_essay-final-gettyimages-685469924.jpg",
+        "https://media.wired.com/photos/636eb5510ae5a121565fd729/4:3/w_1983,h_1487,c_limit/WI110122_FF_ForeverDogs_2400x1350_crop.jpg",
+        "https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/96b0f8c1fc7546deab323b0f6ba9f33a.jpg",
+        "https://d.newsweek.com/en/full/2074004/puppy.jpg?w=1600&h=1600&q=88&f=dedf0208a00ec721b5356a02ef474bf2",
+        "https://www.charlotte.providencevets.com/files/providence-animal-hospital-charlotte-are-rabbits-rodents-blog.jpeg",
+        "https://a-z-animals.com/media/2022/02/spotted-rabbit-in-sunlight-at-the-green-grass-on-the-garden-picture-id1271893635-1024x535.jpg"
+    ]
+}
